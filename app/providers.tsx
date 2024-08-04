836f666675b7c0b6de22513cab4dd868fae2a814
benyasin <492596635@qq.com>
2024-08-04 15:41:50 +0800

    i18n

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import * as React from "react";
-import { useState } from "react";
 import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
