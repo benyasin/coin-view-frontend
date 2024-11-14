e1a270d8459dc820bcaf18e047446e6c9a8e7190
benyasin <492596635@qq.com>
2024-11-14 16:01:23 +0800

    opt

--- app/detail/layout.tsx
+++ app/detail/layout.tsx
@@ -1,24 +1,3 @@
-export const metadata = {
-  title: "Detail Page Title",
-  description: "Description for detail page",
-  openGraph: {
-    title: "Detail Page Title",
-    description: "Description for detail page",
-    images: [
-      {
-        url: "URL to the detail page image",
-        width: 800,
-        height: 800,
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Detail Page Title",
-    description: "Description for detail page",
-    images: ["URL to the detail page image"],
-  },
-};
 export default function DetailLayout({
   children,
 }: {
