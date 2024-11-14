cd18cc5d5d0f4fca8237f4f2d82850f370ddbe04
benyasin <492596635@qq.com>
2024-11-14 11:49:54 +0800

    opt

--- app/detail/layout.tsx
+++ app/detail/layout.tsx
@@ -1,12 +1,4 @@
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Detail - CoinView Today",
-  openGraph: {
-    title: "Detail - CoinView Today",
-    url: "https://www.coinview.today/detail",
-  },
-};
+import Head from "next/head";
 
 export default function DetailLayout({
   children,
@@ -14,8 +6,18 @@ export default function DetailLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section>
-      <div>{children}</div>
-    </section>
+    <>
+      <Head>
+        <title>Detail Page Title</title>
+        <meta name="description" content="Description for detail page" />
+        <meta property="og:title" content="Detail Page Title" />
+        <meta property="og:description" content="Description for detail page" />
+        <meta property="og:image" content="URL to the detail page image" />
+        {/* 添加其他 meta 信息 */}
+      </Head>
+      <section>
+        <div>{children}</div>
+      </section>
+    </>
   );
 }
