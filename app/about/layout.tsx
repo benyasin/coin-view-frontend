3c8974c6068fd01bc70acb4e687d1cee9392bc99
benyasin <492596635@qq.com>
2024-11-22 14:05:44 +0800

    opt

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,12 +1,15 @@
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "About - CoinView Today",
-  openGraph: {
+export async function generateMetadata(props: any) {
+  console.log("generateMetadata: props", props);
+  return {
     title: "About - CoinView Today",
-    url: "https://www.coinview.today/about",
-  },
-};
+    openGraph: {
+      title: "About - CoinView Today",
+      url: "https://www.coinview.today/about",
+    },
+  };
+}
 
 export default function AboutLayout({
   children,
